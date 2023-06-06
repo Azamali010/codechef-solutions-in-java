0ea696dd147b5532cfdd24f27e5ff91c15338d56
Azamali010 <azamali07711@gmail.com>
2023-06-07 02:15:07 +0530

    this is my first commit

---
+++ app/src/main/java/com/example/myapplicationa/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.myapplicationa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var A1 = findViewById<Button>(R.id.A1)
+    }
+
+}
